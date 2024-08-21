a6d3dbdb55ecff6320133adbcca973addb44bce3
jevans10 <37997006+jevans10@users.noreply.github.com>
2024-08-21 15:10:01 -0500

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation</title>
+    <link href="css/webflow-style.css" rel="stylesheet" type="text/css"/>
+    <link href="images/favicon.png" rel="shortcut icon"/>
+</head>
+<body>
+    <div class="navbar-logo-left">
+        <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-logo-left-container shadow-three w-nav">
+            <div class="container">
+                <div class="navbar-wrapper">
+                    <a href="#" class="navbar-brand w-nav-brand">
+                        <img loading="lazy" height="50" alt="Brand Logo" src="images/black-favicon.svg" class="image"/>
+                    </a>
+                    <nav role="navigation" class="nav-menu-wrapper w-nav-menu">
+                        <ul role="list" class="nav-menu-two w-list-unstyled">
+                            <li>
+                                <a href="https://josephineevans.org/JOSEPHINE_EVANS'_RESUME.pdf" class="nav-link">Download my resume</a>
+                            </li>
+                            <li class="mobile-margin-top-10">
+                                <a href="https://jevans10.github.io/portfolio/projects.html" class="nav-link">View my portfolio</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="menu-button w-nav-button">
+                        <div class="w-icon-nav-menu"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/webflow-script.js" type="text/javascript"></script>
+</body>
+</html>
